6f59c88ad1d2055eb73d0dc265da3d9d801665fa
Keifer Erikson <keifer@erikson.dev>
2026-03-01 14:21:31 -0700

    Add project files.

---
+++ IdleOn Wiki/Models/WikiResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace IdleOn_Wiki.Models
+{
+    public class WikiResponse
+    {
+        [JsonPropertyName("parse")]
+        public ParseData Parse { get; set; }
+    }
+
+    public class ParseData
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+        public int PageId { get; set; }
+        public TextData Text { get; set; }
+    }
+
+    public class TextData
+    {
+        [JsonPropertyName("*")]
+        public string HtmlContent { get; set; }
+    }
+
+}
---
+++ IdleOn Wiki/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using IdleOn_Wiki.Models;
+
+namespace IdleOn_Wiki
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Add("User-Agent", "PersonalIdleOnWikiTest/0.1 (https://github.com/keifererikson) .NET/10.0");
+
+            HttpResponseMessage response = await client.GetAsync("https://idleon.wiki/api.php?action=parse&page=Green_Mushroom&format=json");
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            WikiResponse myData = JsonSerializer.Deserialize<WikiResponse>(jsonResponse, options);
+
+            Console.WriteLine(myData);
+            Console.WriteLine(myData.Parse);
+
+            Console.WriteLine($"Monster Name: {myData.Parse.Title}");
+            Console.WriteLine($"Wiki Page ID: {myData.Parse.PageId}");
+            Console.WriteLine($"Length of HTML: {myData.Parse.Text.HtmlContent.Length}");
+
+        }
+    }
+}
