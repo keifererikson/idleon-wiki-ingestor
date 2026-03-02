b96aec55dad9882f0fa0048be06c1435a0fc1d1c
Keifer Erikson <keifer@erikson.dev>
2026-03-01 22:42:38 -0700

    feat: add empty db context

---
+++ IdleOn Wiki/Data/WikiDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdleOn_Wiki.Data
+{
+    internal class WikiDbContext
+    {
+        // TODO: Fill out dbcontext...
+    }
+}
